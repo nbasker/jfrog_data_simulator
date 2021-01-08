1cb9fe745004f8a8033c557532d3de9e29de2d13
Nicholas Basker <nicholasb@jfrog.com>
2021-01-08 19:17:48 +0530

    Create and Delete remote repo, config class to instantiate reference and dut RT managers

--- main.go
+++ main.go
@@ -14,52 +14,16 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/jfrog/jfrog-client-go/artifactory"
 	"github.com/jfrog/jfrog-client-go/artifactory/auth"
+	"github.com/jfrog/jfrog-client-go/artifactory/services"
 	jfauth "github.com/jfrog/jfrog-client-go/auth"
-	//"github.com/jfrog/jfrog-client-go/artifactory/services"
 	//serviceutils "github.com/jfrog/jfrog-client-go/artifactory/services/utils"
 	"github.com/jfrog/jfrog-client-go/config"
 	jflog "github.com/jfrog/jfrog-client-go/utils/log"
 )
-
-// Config struct for insight data simulator
-type Config struct {
-	RefArtiServer struct {
-		ArtiURL      string `yaml:"artiurl"`
-		ArtiUsername string `yaml:"artiusername"`
-		ArtiApikey   string `yaml:"artiapikey"`
-	} `yaml:"refartiserver"`
-	DutArtiServer struct {
-		ArtiURL      string `yaml:"artiurl"`
-		ArtiUsername string `yaml:"artiusername"`
-		ArtiApikey   string `yaml:"artiapikey"`
-	} `yaml:"dutartiserver"`
-}
-
-// NewConfig returns a new decoded Config struct
-func NewConfig(configPath string) (*Config, error) {
-	// Create config structure
-	config := &Config{}
-
-	// Open config file
-	file, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Init new YAML decode
-	d := yaml.NewDecoder(file)
-
-	// Start YAML decoding from file
-	if err := d.Decode(&config); err != nil {
-		return nil, err
-	}
-
-	return config, nil
-}
 
 // ValidateConfigPath just makes sure, that the path provided is a file,
 // that can be read
@@ -74,26 +38,95 @@ func ValidateConfigPath(path string) error {
 	return nil
 }
 
-// ParseFlags will create and parse the CLI flags
-// and return the path to be used elsewhere
-func ParseFlags() (string, error) {
-	// String that contains the configured configuration path
-	var configPath string
+// RtConfig struct for insight data simulator
+type RtConfig struct {
+	ConfigPath    string
+	RtCredentials RtUrlCreds
+}
+type RtUrlCreds struct {
+	RefArtiServer struct {
+		ArtiURL      string `yaml:"artiurl"`
+		ArtiUsername string `yaml:"artiusername"`
+		ArtiApikey   string `yaml:"artiapikey"`
+	} `yaml:"refartiserver"`
+	DutArtiServer struct {
+		ArtiURL      string `yaml:"artiurl"`
+		ArtiUsername string `yaml:"artiusername"`
+		ArtiApikey   string `yaml:"artiapikey"`
+	} `yaml:"dutartiserver"`
+}
 
-	// Set up a CLI flag called "-config" to allow users
-	// to supply the configuration file
-	flag.StringVar(&configPath, "config", "./config.yml", "path to config file")
+// NewRtConfig returns a new decoded RtConfig struct
+func NewRtConfig() (*RtConfig, error) {
+	// Create RT config structure
+	config := &RtConfig{}
+
+	flag.StringVar(&config.ConfigPath, "config", "./config.yaml", "path to config file")
 
 	// Actually parse the flags
 	flag.Parse()
 
 	// Validate the path first
-	if err := ValidateConfigPath(configPath); err != nil {
-		return "", err
+	if err := ValidateConfigPath(config.ConfigPath); err != nil {
+		return config, err
 	}
 
-	// Return the configuration path
-	return configPath, nil
+	return config, nil
+}
+
+// InitRtCreds initializes RT credentials from config file
+func (rc *RtConfig) InitRtCreds() error {
+	// Open config file
+	file, err := os.Open(rc.ConfigPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&rc.RtCredentials); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetRefRtDetails gets the RT credential details
+func (rc *RtConfig) GetRefRtDetails() jfauth.ServiceDetails {
+	refRtDetails := auth.NewArtifactoryDetails()
+	refRtDetails.SetUrl(rc.RtCredentials.RefArtiServer.ArtiURL)
+	refRtDetails.SetApiKey(rc.RtCredentials.RefArtiServer.ArtiApikey)
+	refRtDetails.SetUser(rc.RtCredentials.RefArtiServer.ArtiUsername)
+	return refRtDetails
+}
+
+// GetDutRtDetails gets the RT credential details
+func (rc *RtConfig) GetDutRtDetails() jfauth.ServiceDetails {
+	dutRtDetails := auth.NewArtifactoryDetails()
+	dutRtDetails.SetUrl(rc.RtCredentials.DutArtiServer.ArtiURL)
+	dutRtDetails.SetApiKey(rc.RtCredentials.DutArtiServer.ArtiApikey)
+	dutRtDetails.SetUser(rc.RtCredentials.DutArtiServer.ArtiUsername)
+	return dutRtDetails
+}
+
+// GetRtMgr gets the reference RT manager
+func (rc *RtConfig) GetRtMgr(refRtDetails jfauth.ServiceDetails) (artifactory.ArtifactoryServicesManager, error) {
+	ctx := context.Background()
+	svcConfig, err := config.NewConfigBuilder().
+		SetServiceDetails(refRtDetails).
+		SetThreads(1).
+		SetDryRun(false).
+		SetContext(ctx).
+		Build()
+	if err != nil {
+		return nil, err
+	}
+
+	refRtMgr, err := artifactory.New(&refRtDetails, svcConfig)
+	return refRtMgr, err
 }
 
 type FileStorageInfo struct {
@@ -193,7 +226,7 @@ func GetCachedRemoteRepos(artDetails *jfauth.ServiceDetails) (*[]string, error) 
 	sort.Slice(storageInfoGB, func(i, j int) bool { return storageInfoGB[i].UsedSpaceGB > storageInfoGB[j].UsedSpaceGB })
 
 	for _, r := range storageInfoGB {
-		remoteRepos = append(remoteRepos, r.Key)
+		remoteRepos = append(remoteRepos, strings.ReplaceAll(r.Key, "-cache", ""))
 	}
 	return &remoteRepos, nil
 }
@@ -249,40 +282,63 @@ func main() {
 	jflog.SetLogger(jflog.NewLogger(jflog.INFO, f))
 	jflog.Info("Started data simulator")
 
-	// Generate our config based on the config supplied
-	// by the user in the flags
-	cfgPath, err := ParseFlags()
+	cfg, err := NewRtConfig()
 	if err != nil {
 		jflog.Error("Cli parse failure")
+		os.Exit(-1)
 	}
-	cfg, err := NewConfig(cfgPath)
-	if err != nil {
+	if err := cfg.InitRtCreds(); err != nil {
 		jflog.Error("Config parse failure")
+		os.Exit(-1)
 	}
 
-	rtDetails := auth.NewArtifactoryDetails()
-	rtDetails.SetUrl(cfg.RefArtiServer.ArtiURL)
-	rtDetails.SetApiKey(cfg.RefArtiServer.ArtiApikey)
-	rtDetails.SetUser(cfg.RefArtiServer.ArtiUsername)
+	refRtDetails := cfg.GetRefRtDetails()
+	refRtMgr, err := cfg.GetRtMgr(refRtDetails)
+	refRtVer, err := refRtMgr.GetVersion()
+	jflog.Info("Ref RT Version = ", refRtVer)
+	refRtSvcID, err := refRtMgr.GetServiceId()
+	jflog.Info("Ref RT ServiceId = ", refRtSvcID)
 
-	ctx := context.Background()
+	dutRtDetails := cfg.GetDutRtDetails()
+	dutRtMgr, err := cfg.GetRtMgr(dutRtDetails)
+	dutRtVer, err := dutRtMgr.GetVersion()
+	jflog.Info("DUT RT Version = ", dutRtVer)
+	dutRtSvcID, err := refRtMgr.GetServiceId()
+	jflog.Info("DUT RT ServiceId = ", dutRtSvcID)
 
-	serviceConfig, err := config.NewConfigBuilder().
-		SetServiceDetails(rtDetails).
-		SetThreads(1).
-		SetDryRun(false).
-		SetContext(ctx).
-		Build()
-
-	rtManager, err := artifactory.New(&rtDetails, serviceConfig)
-	rtVersion, err := rtManager.GetVersion()
-	jflog.Info("Artifactory Version = ", rtVersion)
-	rtSvcID, err := rtManager.GetServiceId()
-	jflog.Info("Artifactory ServiceId = ", rtSvcID)
-
-	largeRemoteRepos, err := GetCachedRemoteRepos(&rtDetails)
+	largeRemoteRepos, err := GetCachedRemoteRepos(&refRtDetails)
 	jflog.Info("Num of CACHE repos whose size is > 1GB : ", len(*largeRemoteRepos))
 	jflog.Info(fmt.Sprintf("repo list : %+v", *largeRemoteRepos))
+
+	refRtLargeRemoteRepo, err := refRtMgr.GetRepository((*largeRemoteRepos)[0])
+	jflog.Info(fmt.Sprintf("Ref RT repo list : %+v", refRtLargeRemoteRepo))
+	dutRtLargeRemoteRepo, err := dutRtMgr.GetRepository((*largeRemoteRepos)[0])
+	jflog.Info(fmt.Sprintf("DUT RT repo list : %+v", dutRtLargeRemoteRepo))
+
+	if dutRtLargeRemoteRepo != nil && dutRtLargeRemoteRepo.Key == (*largeRemoteRepos)[0] {
+		jflog.Info(fmt.Sprintf("Large remote repo %s is present in DUT", dutRtLargeRemoteRepo.Key))
+		if err := dutRtMgr.DeleteRepository(dutRtLargeRemoteRepo.Key); err != nil {
+			jflog.Error(fmt.Sprintf("Failed to delete in the DUT large remote repo %s", dutRtLargeRemoteRepo.Key))
+			os.Exit(-1)
+		}
+		jflog.Info(fmt.Sprintf("Pausing after deleting %s in DUT", dutRtLargeRemoteRepo.Key))
+		time.Sleep(5 * time.Second)
+	}
+
+	params := services.NewDockerRemoteRepositoryParams()
+	params.Key = (*largeRemoteRepos)[0]
+	params.Url = "https://registry-1.docker.io/"
+	params.RepoLayoutRef = "simple-default"
+	params.Description = "A caching proxy repository for a registry-1.docker.io"
+	params.XrayIndex = &[]bool{true}[0]
+	params.AssumedOfflinePeriodSecs = 600
+	if err = dutRtMgr.CreateRemoteRepository().Docker(params); err != nil {
+		jflog.Error(fmt.Sprintf("Failed to remote repo %s in DUT", (*largeRemoteRepos)[0]))
+		os.Exit(-1)
+	}
+	dutRtLargeRemoteRepo, err = dutRtMgr.GetRepository((*largeRemoteRepos)[0])
+	jflog.Info(fmt.Sprintf("After recreation DUT RT repo list : %+v", dutRtLargeRemoteRepo))
+
 	/*
 		rr := []string{(*largeRemoteRepos)[0]}
 		for _, r := range rr {
